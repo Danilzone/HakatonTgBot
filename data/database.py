7cd5edf3f09e82f8656bd4d1c0e52cfa048877db
Danilzone <Anton990.bel@gmail.com>
2024-04-06 15:28:28 +0300

    Получаем и записываем значения в бд

--- data/database.py
+++ data/database.py
@@ -1,8 +1,34 @@
 # работа с БД
 from rich import print
+from rich.console import Console
+
 import sqlite3
 
+console = Console()
+
 class WorkDB:
-    def __init__(self):
-        pass
+    def __init__(self, way_db):
+
+        self.conn = sqlite3.connect(way_db)    # подключаемся к базе данных
+        self.c = self.conn.cursor()                 # Создаем курсор - для работы с бд
+
+    def getTest(self):
+
+        res = self.c.execute(' SELECT * FROM `users` ').fetchall()
+        self.conn.commit()
+        print("Получен из бд - " + res)
+
+    def setTest(self, user_id, user_name, user_dogname):
+        self.user_id = user_id
+        self.user_name = user_name
+        self.user_dogname = user_dogname
+
+        try:
+            res = self.c.execute(' INSERT INTO `users` ("user_id", "user_name", "user_dogname") VALUES (?, ?, ?) ', (user_id, user_name, user_dogname,))
+            self.conn.commit()
+            print("Юзер добален!")
+            
+        except Exception:
+            console.print_exception(show_locals=True)
+
 
